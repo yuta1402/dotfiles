8f7911c6c532de0a3f3fd98bf66f710c239c1d6e
guts <yuta1402souja@live.jp>
2019-09-07 23:02:36 +0900

    Add peco installer

---
+++ etc/install-peco.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PREFIX="${HOME}/bin"
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+peco_latest_version=$(get_latest_release "peco/peco")
+
+os_type=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+case "$(uname -m)" in
+    x*)
+        os_machine="amd64"
+        ;;
+    i*)
+        os_machine="386"
+        ;;
+    aarch64)
+        os_machine="arm64"
+        ;;
+    armv6l)
+        os_machine="arm"
+        ;;
+    armv7l)
+        os_machine="arm"
+        ;;
+    *)
+        echo "$(uname -m) didn't match anything machine"
+        exit 1
+esac
+
+url="https://github.com/peco/peco/releases/download/${peco_latest_version}/peco_${os_type}_${os_machine}.tar.gz"
+
+mkdir -p "${PREFIX}"
+
+wget -qO- "${url}" |
+    tar -xvz -C "${PREFIX}" --strip-components 1 peco_linux_amd64/peco
