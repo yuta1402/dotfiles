4978f662a67fe103348109afd05e28b1940de28e
guts <yuta1402souja@live.jp>
2021-05-10 18:26:34 +0900

    Fix pyenv init

--- .zsh/base.zsh
+++ .zsh/base.zsh
@@ -45,7 +45,7 @@ setopt auto_cd
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init - --no-rehash)"
+  eval "$(pyenv init --path --no-rehash)"
   eval "$(pyenv virtualenv-init - --no-rehash)"
 fi
 
