ee2a4a3e176701ba29898f5c6d47e1b06bf3e162
guts <yuta1402souja@live.jp>
2022-08-05 01:27:59 +0900

    Add benchmark

---
+++ scripts/benchmark.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+set -eu
+
+TIMEFMT='%mE';
+
+# warm up
+(time zsh -i -c exit 1) >/dev/null 2>&1
+
+total_millisec=0
+for i in $(seq 1 10); do
+    millisec=$((time zsh -i -c exit) 2>/dev/stdout >/dev/null)
+    millisec=$(echo ${millisec} | tr -d 'ms')
+    total_millisec=$((${total_millisec} + ${millisec}))
+done
+average_millisec=$((${total_millisec} / 10))
+
+cat<<EOF
+[
+    {
+        "name": "zsh load time",
+        "unit": "millisecond",
+        "value": ${average_millisec}
+    }
+]
+EOF
