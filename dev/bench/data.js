fce6ef7299690c2e19fbb30d2394ed8afe9093fd
github-action-benchmark <github@users.noreply.github.com>
2022-08-04 18:16:57 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6040a7a610525f73f08c4c2a683fd7f553de9127

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1659637017528,
+  "repoUrl": "https://github.com/yuta1402/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "committer": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "distinct": true,
+          "id": "6040a7a610525f73f08c4c2a683fd7f553de9127",
+          "message": "Fix result.json",
+          "timestamp": "2022-08-05T03:04:04+09:00",
+          "tree_id": "1c8086cc21f280181203e79e52ceddc839e931d3",
+          "url": "https://github.com/yuta1402/dotfiles/commit/6040a7a610525f73f08c4c2a683fd7f553de9127"
+        },
+        "date": 1659637016309,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": "40.0234861",
+            "unit": "millisecond"
+          }
+        ]
+      }
+    ]
+  }
+}
