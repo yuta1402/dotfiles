e809a2741ead69df940cfeaf22a30e2c3da31d45
guts <yuta1402souja@live.jp>
2022-08-05 23:18:53 +0900

    Add os to benchmark name

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,13 +2,24 @@
 
 set -eu
 
+os=$(uname -s | tr '[A-Z]' '[a-z]')
+case "${os}" in
+    darwin)
+        os='macOS'
+        ;;
+    linux)
+        os='linux'
+        ;;
+    *)
+esac
+
 hyperfine 'zsh -i -c exit' --show-output --warmup 3 --runs 10 -u millisecond --export-json bench.json > /dev/null
 mean_time=$(cat bench.json | jq '.results[0].mean * 1000 | tostring')
 
 cat<<EOF
 [
     {
-        "name": "zsh load time",
+        "name": "zsh load time (${os})",
         "unit": "millisecond",
         "value": ${mean_time}
     }
