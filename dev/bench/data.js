0f025045e2a0f68315d3ae7f7b0c3041dc9251f6
github-action-benchmark <github@users.noreply.github.com>
2022-08-07 04:18:57 +0000

    add Zsh Benchmark (customSmallerIsBetter) benchmark result for 678e12fae385643b0448c6a6c8a708a8368b61e5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,38 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659803717168,
+  "lastUpdate": 1659845937415,
   "repoUrl": "https://github.com/yuta1402/dotfiles",
   "entries": {
-    "Benchmark": [
+    "Benchmark": [],
+    "Zsh Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "committer": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "distinct": true,
+          "id": "678e12fae385643b0448c6a6c8a708a8368b61e5",
+          "message": "Fix bench",
+          "timestamp": "2022-08-07T13:16:38+09:00",
+          "tree_id": "700ac2a091b40279e0e94447d6b8546d81fedd60",
+          "url": "https://github.com/yuta1402/dotfiles/commit/678e12fae385643b0448c6a6c8a708a8368b61e5"
+        },
+        "date": 1659845936883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time (Linux)",
+            "value": "29.93061866",
+            "unit": "millisecond"
+          }
+        ]
+      }
     ]
   }
 }
