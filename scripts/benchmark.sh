9c95d2e26a6938bfa9e5ab181708da4b28fd0361
guts <yuta1402souja@live.jp>
2022-08-07 12:22:12 +0900

    Add nvim benchmark

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,6 +2,22 @@
 
 set -eu
 
+usage()
+{
+    cat <<EOF
+Usage:
+    $(basename "${0}") <command> <name>
+EOF
+}
+
+if [ $# -ne 2 ]; then
+    usage
+    exit 1
+fi
+
+command="${1}"
+name="${2}"
+
 os=$(uname -s | tr '[A-Z]' '[a-z]')
 case "${os}" in
     darwin)
@@ -13,13 +29,13 @@ case "${os}" in
     *)
 esac
 
-hyperfine 'zsh -i -c exit' --show-output --warmup 3 --runs 10 -u millisecond --export-json bench.json > /dev/null
+hyperfine "${command}" --show-output --warmup 3 --runs 10 -u millisecond --export-json bench.json > /dev/null
 mean_time=$(cat bench.json | jq '.results[0].mean * 1000 | tostring')
 
 cat<<EOF
 [
     {
-        "name": "zsh load time (${os})",
+        "name": "${name}",
         "unit": "millisecond",
         "value": ${mean_time}
     }
