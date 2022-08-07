c3b6a6f3c25356d91d1a75426de49f0fab7779c7
github-action-benchmark <github@users.noreply.github.com>
2022-08-07 12:56:53 +0000

    add Neovim Benchmark (customSmallerIsBetter) benchmark result for 88070c8bac3224a587cb42f3ea61b8cb1b5b988e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659877012417,
+  "lastUpdate": 1659877013908,
   "repoUrl": "https://github.com/yuta1402/dotfiles",
   "entries": {
     "Benchmark": [],
@@ -437,6 +437,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "nvim load time (Linux)",
             "value": "161.55374942",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "committer": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "distinct": true,
+          "id": "88070c8bac3224a587cb42f3ea61b8cb1b5b988e",
+          "message": "Fix dir",
+          "timestamp": "2022-08-07T21:53:35+09:00",
+          "tree_id": "3c3375eedea3a9dc2ffc25e8373035b79d5518ff",
+          "url": "https://github.com/yuta1402/dotfiles/commit/88070c8bac3224a587cb42f3ea61b8cb1b5b988e"
+        },
+        "date": 1659877013423,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nvim load time (macOS)",
+            "value": "302.61984464",
             "unit": "millisecond"
           }
         ]
