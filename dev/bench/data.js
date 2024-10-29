d9c31f2dbe5d857724e6f1840a6ca02d35f4ff77
github-action-benchmark <github@users.noreply.github.com>
2024-10-29 05:23:00 +0000

    add Neovim Benchmark (customSmallerIsBetter) benchmark result for e781ea19beaf55b8f435f908896bba54d89f4df7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730179378306,
+  "lastUpdate": 1730179380492,
   "repoUrl": "https://github.com/yuta1402/dotfiles",
   "entries": {
     "Zsh Benchmark": [
@@ -3771,6 +3771,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "nvim load time (Linux)",
             "value": "115.89797242",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "committer": {
+            "email": "yuta1402souja@live.jp",
+            "name": "guts",
+            "username": "yuta1402"
+          },
+          "distinct": true,
+          "id": "e781ea19beaf55b8f435f908896bba54d89f4df7",
+          "message": "Update karabiner.json",
+          "timestamp": "2024-10-29T14:20:32+09:00",
+          "tree_id": "0fc9a367349ac80dbf1fb0e1e7b8e1260ec17a66",
+          "url": "https://github.com/yuta1402/dotfiles/commit/e781ea19beaf55b8f435f908896bba54d89f4df7"
+        },
+        "date": 1730179379460,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nvim load time (macOS)",
+            "value": "189.82207929999998",
             "unit": "millisecond"
           }
         ]
